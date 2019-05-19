0bb3224de6e30f750c997277fe816a948b1888cc
zians <1579072694@qq.com>
2019-05-19 17:37:03 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,73 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navList:['热门推荐','我的关注','问答广场']
+  },
+/**
+ * 获取首页导航数据
+ */
+getNavList(e){
+ // for(let i=0;i<navList.lenth;i++)
+   // e = navList[i]
+},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //获取首页导航数据
+//this.getNavList();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
